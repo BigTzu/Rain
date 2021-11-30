1fb870f7e407cad1ab30c6ea1ac6e001d8afb008
BigTzu <joshuel.kuilwijk@epitech.eu>
2021-12-01 00:27:05 +0900

    add attack animation

--- Player.gd
+++ Player.gd
@@ -8,29 +8,45 @@ const JUMPFORCE = 300
 const ACCEL = 10
 
 var motion = Vector2.ZERO
+var is_attacking = false
+onready var state_machine = $AnimationTree.get("parameters/playback")
 
-func _ready():
-	pass
+func _process(delta):
+	player_movement()
 
-func _physics_process(delta):
-	
+func is_animation_attack_possible(current):
+	if current == "run" or current == "jump" or current == "fall":
+		return false
+	else:
+		return true
+
+func player_movement():
+	var current = state_machine.get_current_node()
 	motion.y += GRAVITY
 	if motion.y > MAXFALLSPEED:
 		motion.y = MAXFALLSPEED
 	
 	motion.x =  clamp(motion.x, -MAXSPEED, MAXSPEED)
 	
+	
+	if Input.is_action_just_pressed("attack") and is_animation_attack_possible(current):
+		state_machine.travel("attack1")
+		return
+	if Input.is_action_just_pressed("big_attack") and is_animation_attack_possible(current):
+		state_machine.travel("attack2 2")
+		return
 	if Input.is_action_pressed("right"):
 		motion.x += ACCEL
-		$Sprite.flip_h = false		
-		$AnimationPlayer.play("Run")
+		$Sprite.flip_h = false
 	elif Input.is_action_pressed("left"):
 		motion.x -= ACCEL
 		$Sprite.flip_h = true
-		$AnimationPlayer.play("Run")
 	else:
 		motion.x = lerp(motion.x, 0, 0.2)
-		$AnimationPlayer.play("Idle")
+		state_machine.travel("idle")
+	
+	if Input.is_action_pressed("right") or Input.is_action_pressed("left"):
+		state_machine.travel("run")
 	
 	if is_on_floor():
 		if Input.is_action_pressed("jump"):
@@ -38,7 +54,7 @@ func _physics_process(delta):
 	
 	if !is_on_floor():
 		if motion.y < 0:
-			$AnimationPlayer.play("Jump")
+			state_machine.travel("jump")
 		elif motion.y > 0:
-			$AnimationPlayer.play("Fall")
+			state_machine.travel("fall")
 	motion = move_and_slide(motion, UP)
