56b8326b0d40cb8e65d3459a5b87cfa74de63e0a
BigTzu <joshuel.kuilwijk@epitech.eu>
2021-12-07 16:05:30 +0900

    done every enemy interaction

--- src/Enemies/Mushroom.gd
+++ src/Enemies/Mushroom.gd
@@ -1,23 +1,85 @@
 extends KinematicBody2D
 
-const WALK_SPEED = 100
-const GRAVITY = 600
+const GRAVITY = 10
+const SPEED = 30
 
+var velocity = Vector2()
+
+var is_moving_right = false
+
+export (int) var health := 30
+
+var direction = 1
+
+var is_dead = false
 onready var player = get_node("/root/World/Player")
 
-func _physics_process(delta):
-	if player:
-		var direction = (player.position - position).normalized()
-		if not is_on_floor():
-			direction.y += GRAVITY
-			
-		move_and_slide(direction * WALK_SPEED)
+func dead():
+	velocity = Vector2(0, 0)
+	$AnimationPlayer.play("Death")
+	if is_dead == true:
+		queue_free()
+	is_dead = true
+	
+func handle_hit(damage):
+	health -= damage
+	$AnimationPlayer.play("Hit")
+	print("enemy was hit, current health: " + str(health))
+	if health <= 0:
+		dead()
+
+func _process(_delta):
+	move_character()
+	detect_turn_around()
+
+func move_character():
+	if $AnimationPlayer.current_animation == "Attack" or $AnimationPlayer.current_animation == "Hit" or is_dead == true:
+		return
+	$AnimationPlayer.play("Run")
+	velocity.x = -SPEED if is_moving_right else SPEED
+	velocity.y += GRAVITY
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func detect_turn_around():
+	if not $RayCast2D.is_colliding() and is_on_floor() or is_on_wall():
+		is_moving_right = !is_moving_right
+		scale.x = -scale.x
+	
+func hit():
+	$AttackDetector.monitoring = true
+
+func end_of_hit():
+	$AttackDetector.monitoring = false
+
+func start_walk():
+	$AnimationPlayer.play("Run")
+	
+func _on_PlayerDetector_body_entered(body: Node) -> void:
+	if body.get_name() == "Player" and is_dead == false:
+		$AnimationPlayer.play("Attack")
+	
+func _on_AtackDetector_body_entered(body: Node) -> void:
+	get_tree().reload_current_scene()
+##   FOLLOW PLAYER AI
+
+# const WALK_SPEED = 100
+# const GRAVITY = 600
+# onready var player = get_node("/root/World/Player")
+
+#func _physics_process(delta):
+	#if player:
+	#	var direction = (player.position - position).normalized()
+	#	if not is_on_floor():
+	#		direction.y += GRAVITY
+	#		
+	#	move_and_slide(direction * WALK_SPEED)
 		
-		$Sprite.set_scale(Vector2(sign(direction.x), 1))
-		$AnimationPlayer.play("Run")
-		
-		for i in get_slide_count():
-			var collision = get_slide_collision(i)
-			var object = collision.collider
+	#	$Sprite.set_scale(Vector2(sign(direction.x), 1))
+	#	$AnimationPlayer.play("Run")
+	#	
+	#	for i in get_slide_count():
+	#		var collision = get_slide_collision(i)
+	#		var object = collision.collider
 			# if object.is_in_group("player"):
 				# object.die()
--- src/player/Player.gd
+++ src/player/Player.gd
@@ -7,8 +7,11 @@ const MAXSPEED = 120
 const JUMPFORCE = 300
 const ACCEL = 10
 
+export (int) var damage := 10
+
 var motion = Vector2.ZERO
 var is_attacking = false
+var change_direction = false
 onready var state_machine = $AnimationTree.get("parameters/playback")
 
 func _process(delta):
@@ -28,7 +31,6 @@ func player_movement():
 	
 	motion.x =  clamp(motion.x, -MAXSPEED, MAXSPEED)
 	
-	
 	if Input.is_action_just_pressed("attack") and is_animation_attack_possible(current):
 		state_machine.travel("attack1")
 		return
@@ -37,10 +39,14 @@ func player_movement():
 		return
 	if Input.is_action_pressed("right"):
 		motion.x += ACCEL
-		$Sprite.flip_h = false
+		if $Sprite.flip_h != false:
+			$Sprite.flip_h = false
+			$Area2D/HitCollision.position.x *= -1
 	elif Input.is_action_pressed("left"):
 		motion.x -= ACCEL
-		$Sprite.flip_h = true
+		if $Sprite.flip_h != true:
+			$Sprite.flip_h = true
+			$Area2D/HitCollision.position.x *= -1
 	else:
 		motion.x = lerp(motion.x, 0, 0.2)
 		state_machine.travel("idle")
@@ -61,3 +67,8 @@ func player_movement():
 
 func die():
 	queue_free()
+
+
+func _on_Area2D_body_entered(body: Node) -> void:
+	if body.has_method("handle_hit"):
+		body.handle_hit(damage)
